50859b0d24a3a1262151d241fe25529f053710e2
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2021-04-11 19:16:14 -0300

    my first commit

---
+++ src/test/java/br/com/erik/heroesapi/HeroesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erik.heroesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
